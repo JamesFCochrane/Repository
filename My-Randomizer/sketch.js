2970a40bed16f4d50c6d2fe18b4c45ec9569cacc
JamesFCochrane <james.cochrane@sjsu.edu>
2022-10-22 02:44:25 -0700

    Update sketch.js

--- My-Randomizer/sketch.js
+++ My-Randomizer/sketch.js
@@ -1,18 +1,18 @@
 let adjectives = [{adjective: "incredible"}, {adjective: "exciting"}, {adjective: "boring"},             
-    // {adjective: "ordinary"}, {adjective: "unrelenting"}, {adjective: "cheap"}, {adjective: "precious"},
-    // {adjective: "so much fun"}, {adjective: "confusing"}, {adjective: "indescribable"}, {adjective: "unfair"},
-    // {adjective: "pointless"}, {adjective: "beautiful"}, {adjective: "disapointing"}, {adjective: "grueling"},
-    // {adjective: "unexplainable"}, {adjective: "unpredictable"}, {adjective: "full of love"}, {adjective: "a test"},
-    // {adjective: "uneventful"}, {adjective: "painful"}, {adjective: "simple"}, {adjective: "overwhelming"},
-    // {adjective: "scary"}, {adjective: "unclean"}, {adjective: "strange"}, {adjective: "odd"}, {adjective: "hell"}, 
-    // {adjective: "a gift"}, {adjective: "uncertain"}, {adjective: "all about money"}, {adjective: "hard"}, 
-    // {adjective: "fragile"}, {adjective: "uncomfortable"}, {adjective: "wet"}, {adjective: "cringe"}, 
-    // {adjective: "whimsical"}, {adjective: "joyous"}, {adjective: "sad"}, {adjective: "fleeting"}, 
-    // {adjective: "ugly"}, {adjective: "unforgiving"}, {adjective: "plain"}, {adjective: "a whirlwind"}, 
-    // {adjective: "tragic"}, {adjective: "ever changing"}, {adjective: "taken for granted"}, {adjective: "fluid"}, 
-    // {adjective: "complicated"}, {adjective: "untintentionally funny"}, {adjective: "rigged"}, 
-    // {adjective: "nerve-racking"}, {adjective: "lonely"}, {adjective: "unpromised"}, {adjective: "a slog"}, 
-    // {adjective: "a marathon"}, {adjective: "nothing special"}, {adjective: "impetuous"}, {adjective: "a joke"}, 
+     //{adjective: "ordinary"}, {adjective: "unrelenting"}, {adjective: "cheap"}, {adjective: "precious"},
+     //{adjective: "so much fun"}, {adjective: "confusing"}, {adjective: "indescribable"}, {adjective: "unfair"},
+     //{adjective: "pointless"}, {adjective: "beautiful"}, {adjective: "disapointing"}, {adjective: "grueling"},
+     //{adjective: "unexplainable"}, {adjective: "unpredictable"}, {adjective: "full of love"}, {adjective: "a test"},
+     //{adjective: "uneventful"}, {adjective: "painful"}, {adjective: "simple"}, {adjective: "overwhelming"},
+     //{adjective: "scary"}, {adjective: "unclean"}, {adjective: "strange"}, {adjective: "odd"}, {adjective: "hell"}, 
+     //{adjective: "a gift"}, {adjective: "uncertain"}, {adjective: "all about money"}, {adjective: "hard"}, 
+     //{adjective: "fragile"}, {adjective: "uncomfortable"}, {adjective: "wet"}, {adjective: "cringe"}, 
+     //{adjective: "whimsical"}, {adjective: "joyous"}, {adjective: "sad"}, {adjective: "fleeting"}, 
+     //{adjective: "ugly"}, {adjective: "unforgiving"}, {adjective: "plain"}, {adjective: "a whirlwind"}, 
+     //{adjective: "tragic"}, {adjective: "ever changing"}, {adjective: "taken for granted"}, {adjective: "fluid"}, 
+     //{adjective: "complicated"}, {adjective: "untintentionally funny"}, {adjective: "rigged"}, 
+     //{adjective: "nerve-racking"}, {adjective: "lonely"}, {adjective: "unpromised"}, {adjective: "a slog"}, 
+     //{adjective: "a marathon"}, {adjective: "nothing special"}, {adjective: "impetuous"}, {adjective: "a joke"}, 
      {adjective: "like a box of chocolates"}, {adjective: "mind-boggling"}, {adjective: "irreplaceable"}, {adjective: "based"}];
 
 let randomIndex;
@@ -44,10 +44,10 @@ function randomizer(){
         randomIndex = int(random(adjectives.length));
         fill(255);
         text(`Life is ${adjectives[randomIndex].adjective}.`, 30, 50);
-       // fill(255);
         adjectives.splice(randomIndex, 1);
     } else {
         fill(255);
+        background(0);
         text("Now you're dead!", 50, 50);
         text("The end.", 50, 85);
     }
